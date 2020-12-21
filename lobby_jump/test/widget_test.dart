bc86633583d78e34a4d0dc92962a283b81ec7595
Mariana Soares <up201605775@g.uporto.pt>
2020-12-21 22:58:24 +0000

    organized code

--- lobby_jump/test/widget_test.dart
+++ lobby_jump/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:lobby_jump/views and controllers/main.dart';
+import 'package:lobby_jump/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
